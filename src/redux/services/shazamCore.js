319e80df47e9e078c693e499fd00299390e81b13
elene <eleneudzilauri@yahoo.com>
2022-10-07 16:57:55 +0400

    Make requests & create Loading/Error components

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '46f56aa7cfmshb9b7c81805476bep1fd933jsn9fda0a5bf6ef');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import playerReducer from './features/playerSlice';
+import { shazamCoreApi } from './services/shazamCore';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const reducers = combineReducers({
+  [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
+  player: playerReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    player: playerReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }).concat(shazamCoreApi.middleware),
 });
+
+export const persistor = persistStore(store);
