0e6408449fe83885ac622005439ae14b42cebb8f
elene <eleneudzilauri@yahoo.com>
2022-10-11 16:24:38 +0400

    top charts page

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+
+const CountryTracks = () => {
+  // const [country, setCountry] = useState('')
+  // const [loading, setLoading] = useState(true)
+  // const { activeSong, isPlaying} = useSelector((state) => state.player)
+
+  // return (
+
+  // )
+};
 
 export default CountryTracks;
+
